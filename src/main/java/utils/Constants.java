d1ec2ea91fe92a70573b7dc06b814dc58402e80e
Alexey Morozov <moroz280294@gmail.com>
2026-01-30 11:51:32 +0300

    добавил константы

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.time.Duration;
+
+public class Constants {
+    public static final String BASE_URL = "https://the-internet.herokuapp.com";
+    public static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(10);
+    public static final String CHROME_BROWSER =   "chrome";
+    public static final String FIREFOX_BROWSER =   "firefox";
+}
